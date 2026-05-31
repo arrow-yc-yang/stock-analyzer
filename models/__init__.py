444724c3bae6d7587409ecfb91513324a7373189
Arrow Yang <arrow_yang@yeah.net>
2026-05-31 13:29:47 +0800

    chore: add factor data model and update exports
    add missing model files to gitignore, initialize sqlalchemy db instance, and add FactorData to model exports

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,14 @@
 """
 数据模型模块
 """
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 from .stock import Stock
 from .kline import KLineData
 from .analysis import AnalysisResult
 from .backtest import BacktestResult
+from .factor_data import FactorData
 
-__all__ = ['Stock', 'KLineData', 'AnalysisResult', 'BacktestResult']
+__all__ = ['Stock', 'KLineData', 'AnalysisResult', 'BacktestResult', 'FactorData']
